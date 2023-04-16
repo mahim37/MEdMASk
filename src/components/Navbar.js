10cb188fe0a3a90c01f0b972094c9c15757e7018
Mahim <mahim.g03@gmail.com>
2023-04-16 16:45:09 +0530

    Added TransferNFT Feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./NavbarStyles.css";
-import { MenuItems } from './MenuItems';
-import { Link } from 'react-router-dom';
+import { MenuItems } from "./MenuItems";
+import { Link } from "react-router-dom";
 export default class Navbar extends Component {
-  state={
-    clicked: false
+  state = {
+    clicked: false,
   };
-  handleClick=()=>{
-    this.setState({clicked: !this.state.clicked});
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
   };
-  render(){ 
-    return(
-      <nav className='NavbarItems'>
-        <h1 className='navbar-logo'>MEdMASk</h1>
-        <div className='menu-icons' onClick={this.handleClick}>
-          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">MedMask</h1>
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
         </div>
         <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-          {MenuItems.map((item,index)=>{
-            return(
+          {MenuItems.map((item, index) => {
+            return (
               <li key={index}>
                 <Link className={item.cName} to={item.url}>
                   <i className={item.icon}></i>
                   {item.title}
                 </Link>
               </li>
-            )
+            );
           })}
-          <Link className='nav-links-new' to='/signup'>
-              <i class="fa-solid fa-user-plus"></i>
-              Sign Up
+          <Link className="nav-links-new" to="/signup">
+            <i class="fa-solid fa-user-plus"></i>
+            Sign Up
           </Link>
         </ul>
       </nav>
-    )
+    );
   }
 }
