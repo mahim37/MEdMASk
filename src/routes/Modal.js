b8c1724d629288c43021dc6118779ce386b5ea39
krishiiitp <98936053+krishiiitp@users.noreply.github.com>
2023-05-26 22:34:40 +0530

    Designed Sign in Page and added checkbox to List my NFT page

--- src/routes/Modal.js
+++ src/routes/Modal.js
@@ -22,9 +22,17 @@ export default function Modal() {
 
   return (
     <>
-      <button onClick={toggleModal} className="btn-modal">
-        Get Started
-      </button>
+      <img src="https://media.istockphoto.com/id/1204743045/photo/medical-technology-concept-remote-medicine-electronic-medical-record.jpg?s=612x612&w=0&k=20&c=WASRAVnJRRItvzWOo8bFgtTamBAt3uIq52GTEaiK-GE="></img>
+      <br></br>
+      <button onClick={toggleModal} class="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-bold text-white transition-all duration-300 bg-indigo-600 rounded-md cursor-pointer group ring-offset-2 ring-1 ring-indigo-300 ring-offset-indigo-200 hover:ring-offset-indigo-500 ease focus:outline-none">
+<span class="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+<span class="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+<span class="relative z-20 flex items-center text-sm">
+<svg class="relative w-5 h-5 mr-2 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+Get Started
+</span>
+</button>
+
       {modal && (
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,29 +1,74 @@
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Modal from "./Modal";
-import "./Modal.css";
+import Footer from "../components/Footer";
+
 function Company() {
-    return (
-        <>
-        <Navbar/>
-        <br></br>
-        <br></br>
-        <div className="thodasahi">
-        <div className="tc">
-            <h1 className="tandc">Terms & Conditions</h1>
-            <ul className="tc">
-                <li>✦  MedMask will collect and store your medical history information in a secure manner. We will only share your information with your doctor or healthcare provider when you provide us with their contact information.</li>
-                <li>✦  Your medical history information will be converted into a non-fungible token (NFT) and stored on a blockchain for security purposes.</li>
-                <li>✦  The NFT containing your medical history information will be accessible only to your doctor or healthcare provider. We will send a copy of the master NFT to your doctor or healthcare provider's designated wallet address.</li>
-                <li>✦  You have the right to access and review your medical history information at any time. You can also request that we delete your medical history information from our database.</li>
-                <li>✦  MedMask does not provide medical advice, diagnosis, or treatment. We are not liable for any medical decisions made by your doctor or healthcare provider based on your medical history information.</li>
-                <li>✦  You are responsible for ensuring the accuracy of your medical history information. We are not liable for any errors or omissions in your medical history information.</li>
-                <li>✦  MedMask reserves the right to modify or terminate our services at any time. We will notify you of any changes or updates to our terms and conditions or privacy policy.</li>
-                <li>✦  You agree to indemnify and hold our company harmless from any claims or damages arising from your use of our services.</li>
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center mt-8" style={{width:'95%',marginLeft:'2.5%',marginRight:'2.5%'}}>
+        <div className="w-1/2 ml-8">
+          <div className="bg-gray-100 rounded p-8">
+            <h1 className="text-2xl font-bold mb-4">Terms & Conditions</h1>
+            <ul className="list-disc pl-4" style={{textAlign:'left'}}>
+              <li className="text-gray-800 mb-2">
+                MedMask will collect and store your medical history information in a secure manner. We will only share your information with your doctor or healthcare provider when you provide us with their contact information.
+              </li>
+              <li className="text-gray-800 mb-2">
+                Your medical history information will be converted into a non-fungible token (NFT) and stored on a blockchain for security purposes.
+              </li>
+              <li className="text-gray-800 mb-2">
+                The NFT containing your medical history information will be accessible only to your doctor or healthcare provider. We will send a copy of the master NFT to your doctor or healthcare provider's designated wallet address.
+              </li>
+              {isExpanded ? (
+                <>
+                  <li className="text-gray-800 mb-2">
+                    You have the right to access and review your medical history information at any time. You can also request that we delete your medical history information from our database.
+                  </li>
+                  <li className="text-gray-800 mb-2">
+                    MedMask does not provide medical advice, diagnosis, or treatment. We are not liable for any medical decisions made by your doctor or healthcare provider based on your medical history information.
+                  </li>
+                  <li className="text-gray-800 mb-2">
+                    You are responsible for ensuring the accuracy of your medical history information. We are not liable for any errors or omissions in your medical history information.
+                  </li>
+                  <li className="text-gray-800 mb-2">
+                    MedMask reserves the right to modify or terminate our services at any time. We will notify you of any changes or updates to our terms and conditions or privacy policy.
+                  </li>
+                  <li className="text-gray-800 mb-2">
+                    You agree to indemnify and hold our company harmless from any claims or damages arising from your use of our services.
+                  </li>
+                    <span
+                      className="text-blue-500 cursor-pointer"
+                      onClick={handleToggle}
+                    >
+                      Read less...
+                    </span>
+                </>
+              ) : (
+                  <span
+                    className="text-blue-500 cursor-pointer"
+                    onClick={handleToggle}
+                  >
+                    Read more...
+                  </span>
+              )}
             </ul>
+          </div>
         </div>
-        <Modal/>
+        <div className="w-1/2 ml-8">
+          <Modal />
         </div>
-        </>
-    )
+      </div>
+      <Footer />
+    </>
+  );
 }
+
 export default Company;
